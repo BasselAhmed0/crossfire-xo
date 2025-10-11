6ab00782044ff1a0eb31b5b35530d24a0e8b94a3
BaSsel Ahmed <88636331+Bensa0@users.noreply.github.com>
2025-10-11 19:37:58 +0300

    Initial commit

---
+++ crossfire-xo-backend/src/main/java/com/Bendata/crossfire_xo/CrossfireXoApplication.java
@@ -0,0 +1,13 @@
+package com.Bendata.crossfire_xo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrossfireXoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrossfireXoApplication.class, args);
+	}
+
+}
---
+++ crossfire-xo-backend/src/test/java/com/Bendata/crossfire_xo/CrossfireXoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Bendata.crossfire_xo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrossfireXoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
